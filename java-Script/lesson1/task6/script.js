4e414bbfe31640d1e4add7162bfd9b67eb279d5d
BogdanLrgrndarniy <bogdanlegendarniyvoin@gmail.com>
2020-11-29 23:09:15 +0300

    FIXED

--- java-Script/lesson1/task6/script.js
+++ java-Script/lesson1/task6/script.js
@@ -1,4 +1,4 @@
 let currencyConverterDollarToEuro = prompt( 'Конвектор валют долары в евро !');
-const dollarToEuro = currencyConverterDollarToEuro * 0.84 ;
+const dollarToEuro = Math.round(currencyConverterDollarToEuro * 0.84) ;
 alert( ` ${dollarToEuro} евро.` );
 alert(message);
