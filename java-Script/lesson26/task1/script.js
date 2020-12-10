2a98b71e836dfca3b03fa177eb1acdb514998fce
BogdanLrgrndarniy <bogdanlegendarniyvoin@gmail.com>
2020-12-11 00:32:46 +0300

    added some js practice tasks

---
+++ java-Script/lesson26/task1/script.js
@@ -0,0 +1,13 @@
+let lastNumber = 15;
+let sum = 0;
+let i = 0;
+while (i <= lastNumber) {
+    sum = sum + i;
+// console.log('i ' + i);
+// console.log('сума ' + sum);
+console.log(`${sum} сума`);
+console.log(`${i} число`);
+  i++;
+
+  }
+  console.log(sum);
